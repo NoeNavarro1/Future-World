0e19015582bd76aeeb81185d88c0a482c8c7d6aa
NoeNavarro1 <noenavarroacevedo92@gmail.com>
2024-02-25 20:09:24 -0600

    09 - Landing Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+import { Description } from "./components/shared/Header/home/Description";
+import { Hero } from "./components/shared/Header/home/Hero";
+import { MainProducts } from "./components/shared/Header/home/MainProducts";
+
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <h1>Hola Mundo xD</h1>
+    <main>
+      <Hero />
+      <Description />
+      <MainProducts />
     </main>
   );
 }
