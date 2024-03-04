08e963f184082159bd3e3330226d5b7345617c44
NoeNavarro1 <noenavarroacevedo92@gmail.com>
2024-03-04 00:08:21 -0600

    optimizacion de imagenes

--- src/components/home/Description/Description.tsx
+++ src/components/home/Description/Description.tsx
@@ -1,9 +1,21 @@
+import styles from './Description.module.sass'
+import Image from  'next/image'
+
+
 export const Description = () => {
     return(
         <section>
-            <img src="/images/description.jpeg" alt="products" />
+            <Image src="/images/description.jpeg" alt="products" 
+            width={500} 
+            height={300}
+            priority= {false}
+            quality={100}
+            />
+
+            <div className={styles.Description__text}>
             <h2>Description</h2>
             <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis inventore assumenda similique perferendis magni excepturi voluptate, natus recusandae delectus quia, accusantium aliquam doloribus ullam repellat dolorem distinctio asperiores eligendi. Repudiandae?</p>
+            </div>
         </section>
     )
 }
