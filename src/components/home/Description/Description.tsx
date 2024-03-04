5957b2bceb57fabf3981175a6fe696aa60506254
NoeNavarro1 <noenavarroacevedo92@gmail.com>
2024-03-03 23:49:08 -0600

    agregar archivos estaticos

--- src/components/home/Description/Description.tsx
+++ src/components/home/Description/Description.tsx
@@ -1,7 +1,9 @@
 export const Description = () => {
     return(
         <section>
-            <h1>Description</h1>
+            <img src="/images/description.jpeg" alt="products" />
+            <h2>Description</h2>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis inventore assumenda similique perferendis magni excepturi voluptate, natus recusandae delectus quia, accusantium aliquam doloribus ullam repellat dolorem distinctio asperiores eligendi. Repudiandae?</p>
         </section>
     )
 }
