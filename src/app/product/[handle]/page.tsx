7a01ec22f38880fdc24cfc6f9111ea5dd53843bd
NoeNavarro1 <noenavarroacevedo92@gmail.com>
2024-03-22 16:30:17 -0600

    34 HTML dinamico

--- src/app/product/[handle]/page.tsx
+++ src/app/product/[handle]/page.tsx
@@ -1,15 +1,22 @@
+
 import { ProductView } from "@/components/product/productView"
 import { getProducts } from "@/services/shopify/products"
-
+import { redirect } from "next/navigation"
 
 interface ProductPageProps {
-    searchParams: {
-        id: string
-    }
+  searchParams: {
+    id: string
+  }
 }
-export default async function ProductPage({ searchParams }: ProductPageProps){
-    const  id = searchParams.id
-    const products = await getProducts(id)
-    const product = products[0]
-    return <ProductView product={product} />
+
+export default async function ProductPage({ searchParams }: ProductPageProps) {
+  const id = searchParams.id
+  const products = await getProducts(id)
+  const product = products[0]
+
+  if(!id){
+    redirect('/')
+  }
+
+  return <ProductView product={product} />
 }
